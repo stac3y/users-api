54b6925a11fdb1793027f33c5794e72693eba374
stac3y <stacy.starina@gmail.com>
2023-09-12 00:28:30 +0300

    chore: update prettier settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ConfigModule } from '@nestjs/config'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
-  imports: [ConfigModule, DatabaseModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ConfigModule, DatabaseModule],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule {}
--- src/database/migrations/20230910221101-init-user-table.js
+++ src/database/migrations/20230910221101-init-user-table.js
@@ -1,48 +1,48 @@
-'use strict';
+'use strict'
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('users', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        unique: true,
-        primaryKey: true,
-      },
-      phone: {
-        type: Sequelize.CHAR(12),
-        allowNull: false,
-        unique: true,
-      },
-      email: {
-        type: Sequelize.CHAR(256),
-        allowNull: false,
-        unique: true,
-      },
-      password: {
-        type: Sequelize.CHAR(64),
-        allowNull: false,
-      },
-      createdAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        default: Date.now(),
-      },
-      updatedAt: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        default: Date.now(),
-      },
-      deletedAt: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
-    });
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('users', {
+            id: {
+                type: Sequelize.INTEGER,
+                allowNull: false,
+                autoIncrement: true,
+                unique: true,
+                primaryKey: true,
+            },
+            phone: {
+                type: Sequelize.CHAR(12),
+                allowNull: false,
+                unique: true,
+            },
+            email: {
+                type: Sequelize.CHAR(256),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.CHAR(64),
+                allowNull: false,
+            },
+            createdAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                default: Date.now(),
+            },
+            updatedAt: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                default: Date.now(),
+            },
+            deletedAt: {
+                type: Sequelize.DATE,
+                allowNull: true,
+            },
+        })
+    },
 
-  async down(queryInterface) {
-    await queryInterface.dropTable('users');
-  },
-};
+    async down(queryInterface) {
+        await queryInterface.dropTable('users')
+    },
+}
